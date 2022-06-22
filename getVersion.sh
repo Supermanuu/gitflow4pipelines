c5bbed69b753ce483ab240472ad9d193ea1e7e8b
Manuel Pascual Lopez <manuel.pascual@sedecal.com>
2022-06-22 16:44:01 +0200

    added version management compose

---
+++ getVersion.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+ENV_FILE='/home/root/workdir/.env'
+
+[ -z "$1" ] || PROJECT_NAME=${1}_
+[ -z "$2" ] || ARCHITECTURE=_${2}
+
+ls $ENV_FILE &>/dev/null || (echo "Env file not found on $ENV_FILE" && exit 1)
+
+PROJECT_VERSION_MAJOR=$(grep -Eo '^PROJECT_VERSION_MAJOR=[0-9]+' $ENV_FILE | grep -Eo '[0-9]+')
+PROJECT_VERSION_MINOR=$(grep -Eo '^PROJECT_VERSION_MINOR=[0-9]+' $ENV_FILE | grep -Eo '[0-9]+')
+PROJECT_VERSION_PATCH=$(grep -Eo '^PROJECT_VERSION_PATCH=[0-9]+' $ENV_FILE | grep -Eo '[0-9]+')
+
+[ -z "$PROJECT_VERSION_MAJOR" \
+    -o -z "$PROJECT_VERSION_MINOR" \
+    -o -z "$PROJECT_VERSION_PATCH" ] && (echo "Bad env file" && exit 2)
+
+echo ${PROJECT_NAME}${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}${ARCHITECTURE}
---
+++ increaseVersion.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+ENV_FILE='/home/root/workdir/.env'
+
+ls $ENV_FILE &>/dev/null || (echo "Env file not found on $ENV_FILE" && exit 1)
+
+PROJECT_VERSION_MAJOR=$(grep -Eo '^PROJECT_VERSION_MAJOR=[0-9]+' $ENV_FILE | grep -Eo '[0-9]+')
+PROJECT_VERSION_MINOR=$(grep -Eo '^PROJECT_VERSION_MINOR=[0-9]+' $ENV_FILE | grep -Eo '[0-9]+')
+PROJECT_VERSION_PATCH=$(grep -Eo '^PROJECT_VERSION_PATCH=[0-9]+' $ENV_FILE | grep -Eo '[0-9]+')
+
+[ -z "$PROJECT_VERSION_MAJOR" \
+    -o -z "$PROJECT_VERSION_MINOR" \
+    -o -z "$PROJECT_VERSION_PATCH" ] && (echo "Bad env file" && exit 2)
+
+PROJECT_VERSION_PATCH=$(expr $PROJECT_VERSION_PATCH + 1)
+
+sed -i "s/PROJECT_VERSION_MAJOR=[0-9]\+/PROJECT_VERSION_MAJOR=$PROJECT_VERSION_MAJOR/g" $ENV_FILE
+sed -i "s/PROJECT_VERSION_MINOR=[0-9]\+/PROJECT_VERSION_MINOR=$PROJECT_VERSION_MINOR/g" $ENV_FILE
+sed -i "s/PROJECT_VERSION_PATCH=[0-9]\+/PROJECT_VERSION_PATCH=$PROJECT_VERSION_PATCH/g" $ENV_FILE
+
+/getVersion.sh "$@"
