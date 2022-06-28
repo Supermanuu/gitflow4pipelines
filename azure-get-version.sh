5e6acdb412537cc5d8cbf152b49d030c9441d8df
Manuel Pascual Lopez <manuel.pascual@sedecal.com>
2022-06-28 08:46:19 +0200

    better reabibility

--- azure-get-version.sh
+++ azure-get-version.sh
@@ -1,11 +1,20 @@
 set -e
+
+# TODO: remove
 echo $FIX $HOTFIX
+
+echo 'Select version increase'
 [ "$FIX" = "True" ] && INCREASE='--fix'
 [ "$HOTFIX" = "True" ] && INCREASE='--hotfix'
 [ -z "$INCREASE" ] && INCREASE='--next'
+
+echo 'Get versions'
 VERSION=$(docker compose run --rm version version.py -p $(Build.Repository.Name))
 NEW_VERSION=$(docker compose run --rm version version.py -p $(Build.Repository.Name) $INCREASE)
+
 echo "Current version: $VERSION"
 echo "Next version: $NEW_VERSION"
+
+echo 'Returns this variables to YAML pipeline'
 echo "##vso[task.setvariable variable=Version;]$VERSION"
 echo "##vso[task.setvariable variable=NewVersion;]$NEW_VERSION"
--- azure-tag-verison.sh
+++ azure-tag-verison.sh
@@ -1,21 +1,26 @@
 set -e
+
 echo "Setting user and email"
 git config --global user.name "Build Service"
 git config --global user.email "buildservice@buildservice.buildservice"
+
 echo "Getting development"
 git checkout development
 git reset --hard origin/development # removes staged and working directory changes
 git clean -f -d # remove untracked
 git pull
+
 echo "Increasing version"
 [ "$FIX" = "True" ] && INCREASE='--fix'
 [ "$HOTFIX" = "True" ] && INCREASE='--hotfix'
 [ -z "$INCREASE" ] && INCREASE='--next'
 NEW_VERSION=$(docker compose run --rm version version.py -p $(Build.Repository.Name) $INCREASE -w)
+
 echo "Committing new version $NEW_VERSION"
 git add .env
 git commit -m "Generated new version $NEW_VERSION [skip ci]"
 git push
+
 echo "Tagging version to $NEW_VERSION"
 git tag -a $NEW_VERSION -m "New version avaliable"
 git push origin $NEW_VERSION
